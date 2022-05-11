ccf4256bb73e08175736af4be7ac5e71892fcd11
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-11 21:56:00 +0000

    adding client file

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require('net');
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-    
-  })
-
-  conn.on("data", (data) =>{
-    console.log(data)
-  })
-
-  conn.setEncoding('utf8');
-
-  return conn;
-};
+const {connect} = require('./client')
 
 console.log('Connecting...');
 connect();
